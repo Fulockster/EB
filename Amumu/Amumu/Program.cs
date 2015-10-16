600a6488c1676af31b1dbf22dfc1ecaeab745f4d
Fulockster <bahamena@live.com>
2015-10-16 07:43:03 +0300

    ayee

---
+++ Amumu/Amumu/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
+
+namespace Amumu
+{
+   public static class Amumu
+    {
+        // for which champ (ignore these notes)
+        public const string ChampName = "Amumu";
+
+        public static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += OnLoadingComplete;
+        }
+
+        private static void OnLoadingComplete(EventArgs args)
+        {
+            if (Player.Instance.ChampionName !+ ChampName)
+            {
+             // champ name confirm (ignore these notes)
+                return;
+            }
+            SpellManager.Initiliaze();
+            SpellManager.Initialize();
+            ModeManager.Initialize();
+
+            Drawing.OnDraw += Drawing_OnDraw;
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            Circle.Draw(Color.Red, SpellManager.Q.Range, Player.Instance.Position);
+            Circle.Draw(Color.Blue, SpellManager.W.Range, Player.Instance.Position);
+            Circle.Draw(Color.Purple, SpellManager.E.Range, Player.Instance.Position);
+            Circle.Draw(Color.GreenYellow, SpellManager.R.Range, Player.Instance.Position);
+        }
+    }
+}
---
+++ Amumu/Amumu/SpellManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Threading;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
+
+namespace Amumu
+{
+   public static class SpellManager
+    {
+        // Spellz type
+        public static Spell.Skillshot Q { get; private set; }
+        public static Spell.Active W { get; private set; }
+        public static Spell.Active E { get; private set; }
+        public static Spell.Active R { get; private set; }
+
+
+        static SpellManager()
+        {
+            Q = new Spell.Skillshot(SpellSlot.Q, 1070, SkillShotType.Linear, 250, 2000, 85);
+            W = new Spell.Active(SpellSlot.W, 295);
+            E = new Spell.Active(SpellSlot.E, 330);
+            R = new Spell.Active(SpellSlot.R, 528);
+            
+
+        }
+
+        public static void Initialize()
+        {
+            // For initializer to do the job -Hellsing
+        }
+    }
+}
